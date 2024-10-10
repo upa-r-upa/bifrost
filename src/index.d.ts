07c6a3722aa26647bd706fedbb740d8fdbddf315
Atralupus <me@atralupus.com>
2024-10-10 23:58:13 +0900

    formatting

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,13 +1,13 @@
 interface Window {
-	chronoWallet: {
-		sign(signer: string, action: string): Promise<string>;
-		signTx(signer: string, unsignedTx: string): Promise<string>;
-		listAccounts(): Promise<
-			{
-				activated: boolean;
-				address: string;
-			}[]
-		>;
-		getPublicKey(address: string): Promise<stirng>;
-	};
+  chronoWallet: {
+    sign(signer: string, action: string): Promise<string>;
+    signTx(signer: string, unsignedTx: string): Promise<string>;
+    listAccounts(): Promise<
+      {
+        activated: boolean;
+        address: string;
+      }[]
+    >;
+    getPublicKey(address: string): Promise<stirng>;
+  };
 }
