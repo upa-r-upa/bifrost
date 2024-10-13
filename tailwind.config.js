77f3afae1ede713b23c0eada6d8bd8e37aee9a00
upa-r-upa <me.uparupa@gmail.com>
2024-10-14 02:28:58 +0900

    Set font and tailwind font config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Ubuntu Mono"', "sans-serif"],
+        special: ["Quantico"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
