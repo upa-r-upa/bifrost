1d6f4e5d9937e0396d6d5040e44b096a15135587
upa-r-upa <me.uparupa@gmail.com>
2024-10-15 01:24:32 +0900

    Fix typo

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
--- src/pages/RequireAuth.tsx
+++ src/pages/RequireAuth.tsx
@@ -3,19 +3,14 @@ import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 export default function RequireAuth() {
   const location = useLocation();
-  const authLoading = true;
+  const authLoading = false;
   const isAuthorized = false;
 
   if (authLoading) {
     return (
-      <div className="fixed top-0 left-0 w-full flex items-center h-full z-30">
-        <div className="w-full h-full bg-black bg-opacity-20 absolute"></div>
-        <div className="text-center w-full ">
-          <span className="loading loading-dots loading-lg"></span>
-
-          <p>Retrieving authentication information.</p>
-          <p>Please wait a moment.</p>
-        </div>
+      <div>
+        <p>Retrieving authentication information.</p>
+        <p>Please wait a moment.</p>
       </div>
     );
   }
