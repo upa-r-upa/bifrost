9585965025e9fdbe73eeb1e71e7d91a9561384a3
upa-r-upa <me.uparupa@gmail.com>
2024-11-26 18:15:34 +0900

    feat: Setting Apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,67 @@
 import { Outlet } from "react-router-dom";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import { useNetworkStore } from "@/store/chrono";
+import { heimdallMimirUrl, odinMimirUrl, Planet } from "@/constants/planet";
+import { useMemo } from "react";
 
 import pixelArt from "@/assets/pixel-art.png";
 
 function App() {
+  const { planet } = useNetworkStore();
+
+  const client = useMemo(() => {
+    let endpoint;
+    switch (planet) {
+      case Planet.ODIN:
+        endpoint = odinMimirUrl;
+        break;
+      case Planet.HEIMDALL:
+        endpoint = heimdallMimirUrl;
+        break;
+      default:
+        endpoint = odinMimirUrl;
+    }
+
+    return new ApolloClient({
+      uri: endpoint,
+      cache: new InMemoryCache({
+        typePolicies: {
+          WorldInformation: {
+            keyFields: ["id"],
+          },
+        },
+      }),
+    });
+  }, [planet]);
+
   return (
-    <div className="relative w-full bg-background h-screen pt-5">
-      <div className="flex justify-center">
-        <div className="w-full max-w-screen-md">
-          <div className="aspect-video border-2 border-selection-background rounded-md shadow-lg overflow-hidden">
-            {/* 터미널 헤더 */}
-            <div className="flex items-center p-2 bg-selection-background text-foreground rounded-t-md">
-              <div className="flex space-x-2">
-                <div className="w-3 h-3 rounded-full bg-red"></div>
-                <div className="w-3 h-3 rounded-full bg-yellow"></div>
-                <div className="w-3 h-3 rounded-full bg-cyan"></div>
+    <ApolloProvider client={client}>
+      <div className="relative w-full bg-background h-screen pt-5">
+        <div className="flex justify-center">
+          <div className="w-full max-w-screen-md">
+            <div className="aspect-video border-2 border-selection-background rounded-md shadow-lg overflow-hidden">
+              {/* 터미널 헤더 */}
+              <div className="flex items-center p-2 bg-selection-background text-foreground rounded-t-md">
+                <div className="flex space-x-2">
+                  <div className="w-3 h-3 rounded-full bg-red"></div>
+                  <div className="w-3 h-3 rounded-full bg-yellow"></div>
+                  <div className="w-3 h-3 rounded-full bg-cyan"></div>
+                </div>
+
+                <div className="mx-auto text-md font-sans text-foreground flex">
+                  <img className="w-5 mr-2" src={pixelArt} />
+                  <span>Bifrost</span>
+                </div>
               </div>
 
-              <div className="mx-auto text-md font-sans text-foreground flex">
-                <img className="w-5 mr-2" src={pixelArt} />
-                <span>Bifrost</span>
+              <div className="p-3 text-white">
+                <Outlet />
               </div>
-            </div>
-
-            <div className="p-3 text-white">
-              <Outlet />
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </ApolloProvider>
   );
 }
 
