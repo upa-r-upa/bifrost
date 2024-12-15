caa244956b35f3034ec50168c813e3966b0773f3
upa-r-upa <me.uparupa@gmail.com>
2024-12-16 00:00:22 +0900

    feat: Add Error page UI

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,3 +1,21 @@
+import TerminalContainer from "@/components/TerminalContainer";
+import ROUTES from "@/constants/routes";
+import { NavLink, useNavigate } from "react-router-dom";
+
 export default function ErrorPage() {
-  return <div>ERROR</div>;
+  const navigate = useNavigate();
+
+  return (
+    <TerminalContainer title="404 Not found" type="sub">
+      <p>Something went wrong.</p>
+
+      <button className="btn mt-3" onClick={() => navigate(0)}>
+        Reload page
+      </button>
+
+      <NavLink to={ROUTES.MAIN}>
+        <button className="btn mt-3">Go to Home</button>
+      </NavLink>
+    </TerminalContainer>
+  );
 }
